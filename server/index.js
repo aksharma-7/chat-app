89eeaefa9e3da47575be349016591ee0d1e8d3de
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-02-08 22:10:58 +0530

    added basic socketio setup

--- server/index.js
+++ server/index.js
@@ -10,6 +10,19 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// run with client
+io.on('connection', (socket) => {
+  console.log('We have a new connection!!!');
+
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User has left!!');
+  });
+});
+
 app.use(router);
 
 server.listen(PORT, () => {
